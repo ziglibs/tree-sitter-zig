d29ef8905ce256c197d495a40cab0b57abec83b7
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2023-04-27 19:19:36 -0400

    gh action instead of pushing generated code

--- build.zig
+++ build.zig
@@ -1,7 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("tree-sitter-zig", .{
-        .source_file = .{ .path = "src/parser.c" },
+    const target = std.zig.CrossTarget.parse(.{
+        .arch_os_abi = b.user_input_options.get("target") orelse @panic("Please specify the target with .target = ..."),
+        .cpu_features = b.user_input_options.get("cpu") orelse @panic("Please specify the target with .target = ..."),
     });
+    const optimize = std.meta.stringToEnum(std.builtin.Mode, b.user_input_options.get("target") orelse @panic("Please specify the optimization with .optimize = ..."));
+
+    var obj = b.addObject(.{
+        .name = "tree-sitter-zig",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    obj.addCSourceFile("src/parser.c", .{});
+    obj.addIncludePath("src");
+
+    b.installArtifact(obj);
 }
