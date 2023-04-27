b0abae54369e88615f26b807463f30eae06c745c
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2023-04-26 23:55:10 -0400

    100% std!

--- grammar.js
+++ grammar.js
@@ -463,7 +463,8 @@ module.exports = grammar({
         // Comments
         container_doc_comment: (_) =>
             token(repeat1(seq("//!", /[^\n]*/, /[ \n]*/))),
-        doc_comment: (_) => token(repeat1(seq("///", /[^\n]*/, /[ \n]*/))),
+        // doc_comment: (_) => token(repeat1(seq("///", /[^\n]*/, /[ \n]*/))),
+        doc_comment: (_) => token(repeat1(/\/\/\/(([^/\n][^\n]*[ \n]*)|)/)),
         line_comment: (_) => token(seq("//", /.*/)),
 
         // Strings
@@ -496,7 +497,7 @@ module.exports = grammar({
         ),
 
         float: $ => prec(10, choice(
-            token(seq("0x", numericWithSeparator(/[0-9a-fA-F]/), ".", numericWithSeparator(/[0-9a-fA-F]/)), optional(seq(/[pP][+\-]?/, numericWithSeparator(/[0-9]/)))),
+            token(seq("0x", numericWithSeparator(/[0-9a-fA-F]/), ".", numericWithSeparator(/[0-9a-fA-F]/), optional(seq(/[pP][+\-]?/, numericWithSeparator(/[0-9]/))))),
             token(seq(numericWithSeparator(/[0-9]/), ".", numericWithSeparator(/[0-9]/), optional(seq(/[eE][+\-]?/, numericWithSeparator(/[0-9]/))))),
             token(seq("0x", numericWithSeparator(/[0-9a-fA-F]/), /[pP]-/, numericWithSeparator(/[0-9]/))),
             token(seq(numericWithSeparator(/[0-9]/), /[eE]-/, numericWithSeparator(/[0-9]/))),
--- test/samples/hello.zig
+++ test/samples/hello.zig
@@ -1,1 +1,14 @@
-const epsilon = 1e-7;
+/// Creates a Formatter type from a format function. Wrapping data in Formatter(func) causes
+/// the data to be formatted using the given function `func`.  `func` must be of the following
+/// form:
+///
+///     fn formatExample(
+///         data: T,
+///         comptime fmt: []const u8,
+///         options: std.fmt.FormatOptions,
+///         writer: anytype,
+///     ) !void;
+///
+pub fn Formatter(comptime format_fn: anytype) type {
+    _ = format_fn;
+}
