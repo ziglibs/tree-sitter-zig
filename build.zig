385af0abbe941e3b1105128c0629e21297c9e46b
Auguste Rame <19855629+SuperAuguste@users.noreply.github.com>
2023-04-27 21:26:26 -0400

    bruh

--- build.zig
+++ build.zig
@@ -1,19 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = std.zig.CrossTarget.parse(.{
-        .arch_os_abi = b.user_input_options.get("target") orelse @panic("Please specify the target with .target = ..."),
-        .cpu_features = b.user_input_options.get("cpu") orelse @panic("Please specify the target with .target = ..."),
-    });
-    const optimize = std.meta.stringToEnum(std.builtin.Mode, b.user_input_options.get("target") orelse @panic("Please specify the optimization with .optimize = ..."));
-
-    var obj = b.addObject(.{
+    var obj = b.addStaticLibrary(.{
         .name = "tree-sitter-zig",
-        .target = target,
-        .optimize = optimize,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
 
-    obj.addCSourceFile("src/parser.c", .{});
+    obj.linkLibC();
+    obj.addCSourceFile("src/parser.c", &.{});
     obj.addIncludePath("src");
 
     b.installArtifact(obj);
